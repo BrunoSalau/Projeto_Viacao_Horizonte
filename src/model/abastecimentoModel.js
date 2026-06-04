5ad07feb7424245f391716b7a8c0df7fa287aecf
Luiz Henrique <luiz.up.work@gmail.com>
2026-06-04 16:51:41 -0300

    Coloquei class nos models

--- src/model/abastecimentoModel.js
+++ src/model/abastecimentoModel.js
@@ -1,36 +1,38 @@
 import { pool } from '../config/db.js'
+export class modelAbastecimento{
 
-export async function modelCriarAbastecimento(id_veiculo, litros, valor, data_abastecimento
-){
-
-    const result = await pool.query(
-        `INSERT INTO abastecimento
-        (id_veiculo, litros, valor, data_abastecimento)
-        VALUES ($1, $2, $3, $4)
-        RETURNING *`,
-        [id_veiculo, litros, valor, data_abastecimento]
-    );
-
-    return result.rows[0];
-}
-
-export async function modelListarAbastecimentos(){
-
-    const result = await pool.query(
-        `SELECT * FROM abastecimento`
-    );
-
-    return result.rows;
-}
-
-export async function modelDeletarAbastecimento(id){
-
-    const result = await pool.query(
-        `DELETE FROM abastecimento
-        WHERE id = $1
-        RETURNING *`,
-        [id]
-    );
-
-    return result.rows[0];
+    static async criarAbastecimento(id_veiculo, litros, valor, data_abastecimento
+    ){
+    
+        const result = await pool.query(
+            `INSERT INTO abastecimento
+            (id_veiculo, litros, valor, data_abastecimento)
+            VALUES ($1, $2, $3, $4)
+            RETURNING *`,
+            [id_veiculo, litros, valor, data_abastecimento]
+        );
+    
+        return result.rows[0];
+    }
+    
+    static async listarAbastecimentos(){
+    
+        const result = await pool.query(
+            `SELECT * FROM abastecimento`
+        );
+    
+        return result.rows;
+    }
+    
+    static async deletarAbastecimento(id){
+    
+        const result = await pool.query(
+            `DELETE FROM abastecimento
+            WHERE id = $1
+            RETURNING *`,
+            [id]
+        );
+    
+        return result.rows[0];
+    }
 }
--- src/model/viagemModel.js
+++ src/model/viagemModel.js
@@ -1,35 +1,37 @@
 import { pool } from '../config/db.js'
+export class modelViagem{
 
-export async function modelCriarViagem(id_veiculo, id_rota, data_viagem){
-
-    const result = await pool.query(
-        `INSERT INTO viagem
-        (id_veiculo, id_rota, data_viagem)
-        VALUES ($1, $2, $3)
-        RETURNING *`,
-        [id_veiculo, id_rota, data_viagem]
-    );
-
-    return result.rows[0];
-}
-
-export async function modelListarViagens(){
-
-    const result = await pool.query(
-        `SELECT * FROM viagem`
-    );
-
-    return result.rows;
-}
-
-export async function modelDeletarViagem(id){
-
-    const result = await pool.query(
-        `DELETE FROM viagem
-        WHERE id = $1
-        RETURNING *`,
-        [id]
-    );
-
-    return result.rows[0];
+    static async criarViagem(id_veiculo, id_rota, data_viagem){
+    
+        const result = await pool.query(
+            `INSERT INTO viagem
+            (id_veiculo, id_rota, data_viagem)
+            VALUES ($1, $2, $3)
+            RETURNING *`,
+            [id_veiculo, id_rota, data_viagem]
+        );
+    
+        return result.rows[0];
+    }
+    
+    static async listarViagens(){
+    
+        const result = await pool.query(
+            `SELECT * FROM viagem`
+        );
+    
+        return result.rows;
+    }
+    
+    static async deletarViagem(id){
+    
+        const result = await pool.query(
+            `DELETE FROM viagem
+            WHERE id = $1
+            RETURNING *`,
+            [id]
+        );
+    
+        return result.rows[0];
+    }
 }
